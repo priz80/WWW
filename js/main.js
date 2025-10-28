1bde3be12699e8aef831272f7c9bbe77de4fed0e
priz80 <priz8090@gmail.com>
2025-10-28 12:57:29 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-  const swiper = new Swiper('.swiper', {
-  loop: true,
-  centeredSlides: true,
-  
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
