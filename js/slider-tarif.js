56f181b8b0269d22703ae6fba836844feef3fc0c
priz80 <priz8090@gmail.com>
2025-10-31 10:12:20 +0300

    Update slider-tarif.js

--- js/slider-tarif.js
+++ js/slider-tarif.js
@@ -12,7 +12,9 @@ const totalSlideslengh = slidestarif.length;
 // Индекс текущего слайда
 let ex = 0;
 slidestarif[ex].style.transform = `scale(0.8)`;
+/* slidestarif[ex].classList.add('over'); */
 slidestarif[ex+1].style.transform = `scale(1)`;
+slidestarif[ex+1].classList.remove('over');
 
 // Функция обновления позиции
 function updateSliders() {
@@ -26,16 +28,19 @@ prev.addEventListener("click", () => {
     ex = (ex - 1 + totalSlideslengh) % totalSlideslengh;
   } else {
     slidestarif[ex+1].style.transform = `scale(0.8)`;
+    slidestarif[ex+1].classList.add('over');
     ex = 5;
   }
 
   if (ex < 6) {
     slidestarif[ex + 2].style.transform = `scale(0.8)`;
+    slidestarif[ex+2].classList.add('over');
     slidestarif[ex + 2].style.transition = "transform 0.5s ease-in-out";
   }
 
   if (ex < 7) {
     slidestarif[ex+1].style.transform = `scale(1)`;
+    slidestarif[ex+1].classList.remove('over');
     slidestarif[ex+1].style.transition = "transform 0.5s ease-in-out";
   }
 
@@ -45,17 +50,21 @@ prev.addEventListener("click", () => {
 next.addEventListener("click", () => {
   if (ex < 5) {
     slidestarif[ex+1].style.transform = `scale(0.8)`;
+    slidestarif[ex+1].classList.add('over');
     ex = (ex + 1) % totalSlideslengh;
   } else {
     slidestarif[ex+1].style.transform = `scale(0.8)`;
+    slidestarif[ex+1].classList.add('over');
     ex = 0;
   }
 
   if (ex < 7) {
     slidestarif[ex+1].style.transform = `scale(1)`;
+    slidestarif[ex+1].classList.remove('over');
     slidestarif[ex+1].style.transition = "transform 0.5s ease-in-out";
   } else {
     slidestarif[ex - 2].style.transform = `scale(0.8)`;
+    slidestarif[ex+1].classList.add('over');
   }
   updateSliders();
 });
